7fed5b387a43263ebc3b6878e914dcaba6cfbbb5
AnnaGorbunova1 <annagorbunova5@gmail.com>
2023-03-10 20:37:09 +0300

    some refactoring

--- djangotest/models.py
+++ djangotest/models.py
@@ -6,8 +6,7 @@ from django.dispatch import receiver
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    parent_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-    # url = models.CharField(max_length=255, verbose_name='Link', blank=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -20,8 +19,8 @@ class Item(models.Model):
 
 # таблица closure table для построения иерархической структуры
 class ItemsTree(models.Model):
-    parent_id = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='itemstree_parent', null=True, blank=True)
-    child_id = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='itemstree_child')
+    parent = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='itemstree_parent', null=True, blank=True)
+    child = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='itemstree_child')
     depth = models.IntegerField()
 
 
@@ -30,21 +29,21 @@ class ItemsTree(models.Model):
 def create_closure(sender, instance, **kwargs):
 
     # delete old closures
-    ItemsTree.objects.filter(child_id=instance).delete()
+    ItemsTree.objects.filter(child=instance).delete()
 
     # closure on self
-    closure = ItemsTree.objects.create(parent_id=instance, child_id=instance, depth=0)
+    closure = ItemsTree.objects.create(parent=instance, child=instance, depth=0)
     closure.save()
 
-    if instance.parent_id:
+    if instance.parent:
         # closure on nearest parent
-        closure = ItemsTree.objects.create(parent_id=instance.parent_id, child_id=instance, depth=1)
+        closure = ItemsTree.objects.create(parent=instance.parent, child=instance, depth=1)
         closure.save()
 
         # closure on other parents
-        parents = ItemsTree.objects.filter(child_id=instance.parent_id, depth__gt=0)
+        parents = ItemsTree.objects.filter(child=instance.parent, depth__gt=0)
         for p in parents:
             # print(p.parent_id.name, p.child_id.name, p.depth)
-            closure = ItemsTree.objects.create(parent_id=p.parent_id, child_id=instance, depth=p.depth+1)
+            closure = ItemsTree.objects.create(parent=p.parent, child=instance, depth=p.depth + 1)
             closure.save()
 
--- djangotest/views.py
+++ djangotest/views.py
@@ -9,12 +9,12 @@ def index(request):
 
     # получаем список элементов меню, у которых нет родителя
     # значит это наши названия меню
-    roots = Item.objects.filter(parent_id__isnull=True)
+    root_menu_items = Item.objects.filter(parent_id__isnull=True)
 
     main_menu = []
-    for rr in roots:
-        it = Item.objects.get(name=rr.name)
-        main_menu.append({'id': it.id, 'name': it.name})
+    for menu_item in root_menu_items:
+        menu_item_data = Item.objects.get(name=menu_item.name)
+        main_menu.append({'id': menu_item_data.id, 'name': menu_item_data.name})
 
     data = {'main_menu': main_menu}
     return TemplateResponse(request, "index.html", data)
@@ -25,32 +25,32 @@ def show(request, id):
     # дерево предков
     # и ближайшие потомки
     items_list = Item.objects.raw(
-        'SELECT items.id id, itemparent.name as showname, tree.parent_id_id parent, tree.depth depth, 1 as isparent\
+        'SELECT items.id id, itemparent.name as show_name, tree.parent_id parent_id, tree.depth depth, 1 as is_parent\
         FROM djangotest_item items\
         JOIN djangotest_itemstree tree\
-        ON items.id=tree.child_id_id \
+        ON items.id=tree.child_id \
         JOIN djangotest_item itemparent\
-        ON itemparent.id = tree.parent_id_id\
+        ON itemparent.id = tree.parent_id\
         WHERE items.id=%s\
         UNION\
-        SELECT items.id id, items.name as showname, items.parent_id_id parent, 1 depth, 0 as isparent\
+        SELECT items.id id, items.name as show_name, items.parent_id parent_id, 1 depth, 0 as is_parent\
         FROM djangotest_item items\
-        WHERE items.parent_id_id=%s\
-        ORDER by isparent DESC, depth DESC',
+        WHERE items.parent_id=%s\
+        ORDER by is_parent DESC, depth DESC',
         [id, id]
     )
 
     # строим дерево меню
     def get_menu_tree(lst):
-        level = {'name': lst[0].showname, 'id': lst[0].parent}
+        level = {'name': lst[0].show_name, 'id': lst[0].parent_id}
         if len(lst) > 1:
-            if lst[1].isparent:
+            if lst[1].is_parent:
                 level['children'] = []
                 level['children'].append(get_menu_tree(lst[1:]))
             else:
                 level['children'] = []
                 for ls in lst[1:]:
-                    level['children'].append({'name': ls.showname, 'id': ls.id, 'children': {}})
+                    level['children'].append({'name': ls.show_name, 'id': ls.id, 'children': {}})
 
         else:
             level['children'] = {}
@@ -58,8 +58,7 @@ def show(request, id):
         return level
 
     menu = get_menu_tree(items_list)
+    name = Item.objects.get(id=id)
 
-    print(menu)
-
-    data = {'menu': menu}
+    data = {'menu': menu, 'name': name}
     return TemplateResponse(request,  "show.html", data)
